d774420bdf1e3d3c7ef90f8f1e3b78bf64593192
Andrija Matosic <56841259+a-matos00@users.noreply.github.com>
2020-08-25 18:24:52 +0200

    Solved the infinite button spawn issue

--- WEB/web.js
+++ WEB/web.js
@@ -6,13 +6,18 @@ var a = document.createElement('a');
       
       a.href = "https://github.com/a-matos00";
 
-
+var hi_lo = document.createElement("BUTTON");
+	hi_lo.classList.add("project_buttons");
+	hi_lo.innerHTML = "Higher or lower game</br>";
+	
 function homepage(){
+	hi_lo.remove(); //removes the button
 	document.getElementById("text").innerHTML = "Hi my name is Andrija. I am a 20 year old computer science student at the University of Split. I started programming at the age of 18 when I wanted to make my custom website on Blogger. That lead me to HTML, CSS and Javascript. After 2 years of messing around I created this webpage which can hopefully showcase what I learned so far. "
 	document.getElementById("left").innerHTML = "About</br>Me"
 }
 
 function info(){
+	hi_lo.remove(); //removes the button
 	document.getElementById("text").innerHTML = ""; //prevents infinite appending
 	document.getElementById("text").innerHTML = "e-mail: andrija.matosic@outlook.com</br>"
 	document.getElementById("text").innerHTML += "Github: ";
@@ -22,8 +27,8 @@ function info(){
 }
 
 function projects(){
-	document.getElementById("text").innerHTML = ""
-	document.getElementById("left").innerHTML = "My</br>Projects"
-	document.getElementById("text").innerHTML += "Higher or lower game</br>"
-	document.getElementById("text").innerHTML += "Responsive web page"
+	document.getElementById("right").appendChild(hi_lo); //adds the button for the HI-LO game
+	hi_lo.onclick = function(){};
+	document.getElementById("text").innerHTML = "";
+	document.getElementById("left").innerHTML = "My</br>Projects";
 }
